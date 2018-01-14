242c8fb52ba39123f3bd5a15a327295508699030
ricky-hu <rckyhu@gmail.com>
2018-01-13 16:44:10 -0800

    Added intial self reporting analysis files

---
+++ Analytics/selfReportAnalysis.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 13 16:19:29 2018
+
+selfReportAnalysis.py
+
+Analyzes entries of self reporting records to provide physicians with
+visualizations of data and alerts if analysis shows imminent danger to patient.
+
+Input: CSV records of patient's self reporting records
+Output: Graphs of health trends and email notifications of for extreme trends
+
+"""
+
+import matplotlib
